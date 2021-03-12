7243bb03048b7937e1537ef594c74e9b7aca0875
flumine2 <froggt25@gmail.com>
2021-03-12 13:51:08 +0200

    added cw with "="

--- Joined Second Lab/Output/Output.cs
+++ Joined Second Lab/Output/Output.cs
@@ -7,6 +7,7 @@ namespace Joined_Second_Lab
         public void outArray(int[] array)
         {
             Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine("==================================");
         }
 
         public void outArray(int[][] array) 
@@ -19,6 +20,7 @@ namespace Joined_Second_Lab
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("==================================");
         }
 
         public void outArray(int[,] array)
@@ -31,6 +33,7 @@ namespace Joined_Second_Lab
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("==================================");
         }
     }
 }
